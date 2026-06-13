5bbb40cead3097fe08e25989856a45f8216f4c1d
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-06-13 23:11:44 +0530

    schema-database

---
+++ storage/src/schema.rs
@@ -0,0 +1,24 @@
+//  base DDL query to setup the sql database from scratch
+
+pub const INIT_SQL: &str = "
+    CREATE TABLE IF NOT EXISTS files (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        path TEXT NOT NULL UNIQUE
+    );
+    CREATE TABLE IF NOT EXISTS symbols (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        file_id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        kind TEXT NOT NULL,
+        line_number INTEGER NOT NULL,
+        FOREIGN KEY(file_id) REFERENCES files(id) ON DELETE CASCADE
+    );
+    CREATE TABLE IF NOT EXISTS edges (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        source_symbol_id INTEGER NOT NULL,
+        target_symbol_id INTEGER NOT NULL,
+        kind TEXT NOT NULL,
+        FOREIGN KEY(source_symbol_id) REFERENCES symbols(id) ON DELETE CASCADE,
+        FOREIGN KEY(target_symbol_id) REFERENCES symbols(id) ON DELETE CASCADE
+    );
+";
