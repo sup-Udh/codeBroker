f2340e40ac731333bb794a4d943bc59e5e192562
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-06-14 09:16:14 +0530

    parser-toolling functions

---
+++ parser/src/treesitter.rs
@@ -0,0 +1,13 @@
+use tree_sitter::{Parser, Tree};
+
+pub fn parse_rust_code(source_code: &str) -> Option<Tree> {
+    // new tree-sitter parser init
+    let mut parser = Parser::new();
+
+    let rust_language = tree_sitter_rust::language();
+
+    parser.set_language(&rust_language).expect("error Loading the rust grammar");
+
+    parser.parse(source_code, None) // none - optional for previus parsing
+
+}
