f8814b68e2441fcd4bc97fd59c3f845000fa2b3a
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-06-15 07:03:25 +0530

    cargo-checkpass

--- parser/src/extractor.rs
+++ parser/src/extractor.rs
@@ -57,7 +57,7 @@ pub fn extract_imports(tree: &Tree, source_code: &str) -> Vec<ImportNode> {
     let mut imports = Vec::new();
     // The query: Find a `use_declaration`, and capture whatever is inside 
     // its `argument` block and tag it as `@import`.
-    let query_str = "(use_declaration argument: identifier @import)";
+    let query_str = "(use_declaration argument: (identifier) @import)";
 
 
     let language = tree_sitter_rust::language();
